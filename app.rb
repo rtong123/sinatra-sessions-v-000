4d2e71062b2cb0520c33e495ebefc3baa07b8027
Rebecca Tong <rtong@buffalo.edu>
2018-07-16 01:35:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -38,12 +38,12 @@ class App < Sinatra::Base
   get '/set_session' do
     #set session id here
 
-    if session[:id] == 1
+    session[:id] = 1
       # "Session ID set. It's currently set to #{session[:id]}."
-      redirect '/fetch_session_id'
-    else
-      "Session ID has not been set!"
-    end
+    #   redirect '/fetch_session_id'
+    # else
+    #   "Session ID has not been set!"
+    # end
   end
 
   get '/fetch_session_id' do
